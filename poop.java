3828e011f8786e5bbe065d6907965d68f365b024
Brron89 <42758264+Brron89@users.noreply.github.com>
2018-12-03 20:18:31 -0600

    Added new functionallity

--- poop.java
+++ poop.java
@@ -1,5 +1,21 @@
+import java.util.*;
 public class poop{
   public static void main(String[] args) {
-    System.out.println("poop");
+
+    String a;
+    Scanner input = new Scanner(System.in);
+    System.out.println("do you have to poop? (yes or no) ");
+    a = input.next();
+
+    if(a.equals("yes"))
+    {
+      System.out.println("Go to the bathroom");
+    }
+    else
+    {
+      System.out.println("Check back later");
+    }
+    //System.out.println("poop");
+
   }
 }
